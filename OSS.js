56678e59370a7567ec76348ee8ce5a414342dde9
Steve Desmond <steve@vtsv.ca>
2021-10-18 11:36:57 -0400

    Style/layout tweaks

---
+++ OSS.js
@@ -0,0 +1,46 @@
+const data = [
+    {
+        name: "LoadTestToolbox",
+        package: "LoadTestToolbox",
+        type: ".NET"
+    },
+    {
+        name: "xunit.ts",
+        package: "xunit.ts",
+        type: "Node"
+    },
+    {
+        name: "BenchmarkMockNet",
+        type: ".NET"
+    },
+    {
+        name: "php-libyear",
+        package: "ecoapm/libyear",
+        type: "PHP"
+    },
+    {
+        name: "SrcSet",
+        package: "srcset",
+        type: ".NET"
+    },
+    {
+        name: "NotoSans",
+        package: "notosans",
+        type: "Node",
+        tests: false
+    },
+    {
+        name: "GitHubLabelSync",
+        package: "GitHubLabelSync",
+        type: ".NET"
+    },
+    {
+        name: "TompkinsCOVID",
+        type: ".NET"
+    },
+    {
+        name: "ecoAPM.com",
+        CI: false,
+        tests: false
+    }
+];
