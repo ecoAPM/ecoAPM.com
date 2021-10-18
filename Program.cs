3bd3fd4728e0e482c2c52a1b058a174ad3bba757
Steve Desmond <steve@vtsv.ca>
2021-10-18 13:40:56 -0400

    Deploy via GitHub Actions workflow

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Statiq.App;
+using Statiq.Common;
 using Statiq.Web;
 
 namespace ecoAPM.com
@@ -9,6 +10,7 @@ namespace ecoAPM.com
 		public static async Task Main(string[] args)
 			=> await Bootstrapper.Factory
 				.CreateWeb(args)
+				.DeployToGitHubPages("ecoAPM", "ecoAPM.com", Config.FromSetting<string>("GITHUB_TOKEN"))
 				.RunAsync();
 	}
 }
