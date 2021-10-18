f1f85578d904a8286d119257e77d72bb9e00d3a4
Steve Desmond <steve@vtsv.ca>
2021-10-18 13:24:46 -0400

    Initial conversion to statiq

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Web;
+
+namespace ecoAPM.com
+{
+	public static class Program
+	{
+		public static async Task Main(string[] args)
+			=> await Bootstrapper.Factory
+				.CreateWeb(args)
+				.RunAsync();
+	}
+}
