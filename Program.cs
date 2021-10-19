02235096b5018be86947225cbec879271901406f
Steve Desmond <steve@vtsv.ca>
2021-10-19 12:19:56 -0400

    Use directory-style URL formatting

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 using Statiq.App;
 using Statiq.Common;
+using Statiq.Core;
 using Statiq.Web;
 
 namespace ecoAPM.com
@@ -10,7 +13,29 @@ namespace ecoAPM.com
 		public static async Task Main(string[] args)
 			=> await Bootstrapper.Factory
 				.CreateWeb(args)
+				.ModifyPipeline("Content", SetURL)
 				.DeployToGitHubPages("ecoAPM", "ecoAPM.com", Config.FromSetting<string>("GITHUB_TOKEN"))
 				.RunAsync();
+				
+		private static void SetURL(IPipeline pipeline)
+			=> pipeline.ProcessModules.Add(SetDestination());
+
+		private static SetDestination SetDestination()
+			=> new SetDestination(ConfigPath());
+
+		private static Config<NormalizedPath> ConfigPath()
+			=> Config.FromDocument<NormalizedPath>(NicePath);
+
+		private static NormalizedPath NicePath(IDocument document)
+			=> new NormalizedPath(Path(document));
+		
+		private static string Path(IDocument document)
+			=> System.IO.Path.Join(Directory(document), "index.html");
+		
+		private static string Directory(IDocument document)
+			=> Directory(document.Source.FileNameWithoutExtension.ToString());
+
+		private static string Directory(string filename)
+			=> filename == "index" ? string.Empty : filename;
 	}
 }
