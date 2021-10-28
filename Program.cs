de5941274a9e4ddc692a11fd9f803be8d5c6291c
Steve Desmond <steve@vtsv.ca>
2021-10-28 15:37:00 -0400

    Convert static assets to build-generated

--- Program.cs
+++ Program.cs
@@ -1,45 +1,38 @@
-﻿using System;
-using System.IO;
 using System.Threading.Tasks;
+using ecoAPM.StatiqPipelines;
+using SrcSet.Statiq;
 using Statiq.App;
 using Statiq.Common;
-using Statiq.Core;
 using Statiq.Web;
 
-namespace ecoAPM.com
+namespace ecoAPM.Site
 {
 	public static class Program
 	{
 		public static async Task Main(string[] args)
 			=> await Bootstrapper.Factory
-				.CreateWeb(args)
-				.ModifyPipeline("Content", SetURL)
+				.CreateDefaultWithout(args, DefaultFeatures.Pipelines)
+				.AddWeb()
+				.ModifyPipeline("Content", NiceURLs)
+				.AddPipeline("SrcSet", ResponsiveImages)
+				.AddPipeline("NPM", NodeModules)
 				.DeployToGitHubPages("ecoAPM", "ecoAPM.com", Config.FromSetting<string>("GITHUB_TOKEN"))
 				.RunAsync();
 
-		private static void SetURL(IPipeline pipeline)
-			=> pipeline.ProcessModules.Add(SetDestination());
+		private static void NiceURLs(IPipeline pipeline)
+			=> pipeline.ProcessModules.Add(new NiceURL());
 
-		private static SetDestination SetDestination()
-			=> new SetDestination(ConfigPath());
+		private static IPipeline NodeModules(IReadOnlySettings arg)
+			=> new CopyFromNPM(new [] {
+						"bootstrap/dist/css/bootstrap.min.css",
+					 	"bootstrap/dist/js/bootstrap.min.js",
+						"jquery/dist/jquery.min.js",
+						"marked/marked.min.js",
+						"notosans/*",
+						"vue/dist/vue.global.prod.js"
+			});
 
-		private static Config<NormalizedPath> ConfigPath()
-			=> Config.FromDocument<NormalizedPath>(NicePath);
-
-		private static NormalizedPath NicePath(IDocument document)
-			=> new NormalizedPath(Path(document));
-
-		private static string Path(IDocument document)
-			=> System.IO.Path.Join(Directory(document), "index.html");
-
-		private static string Directory(IDocument document)
-			=> Directory(document.Source.GetRelativeInputPath());
-
-		private static string Directory(NormalizedPath path)
-			=> path.ChangeExtension("")
-				.ToString()
-				.TrimEnd('.')
-				.Replace("index", "")
-				.Replace("//", "/");
+		private static IPipeline ResponsiveImages(IReadOnlySettings arg)
+			=> new ResponsiveImages("**/*.jpg");
 	}
 }
