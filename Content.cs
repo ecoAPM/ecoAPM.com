218c52c6bc1b97adc10d1676e55d1c3d1dbcf8e1
Steve Desmond <steve@vtsv.ca>
2021-11-15 12:28:29 -0500

    Use file-scoped namespaces

--- Content.cs
+++ Content.cs
@@ -3,60 +3,59 @@ using System.Linq;
 using System.Threading.Tasks;
 using Statiq.Common;
 
-namespace ecoAPM.Site
+namespace ecoAPM.Site;
+
+public static class Content
 {
-	public static class Content
-	{
-		public static IEnumerable<IDocument> Software(IPipelineOutputs output)
-			=> Directory(output, "software");
+	public static IEnumerable<IDocument> Software(IPipelineOutputs output)
+		=> Directory(output, "software");
 
-		public static IEnumerable<IDocument> Services(IPipelineOutputs output)
-			=> Directory(output, "services");
+	public static IEnumerable<IDocument> Services(IPipelineOutputs output)
+		=> Directory(output, "services");
 
-		public static IEnumerable<IDocument> BlogPosts(IPipelineOutputs output)
-			=> Directory(output, "blog");
+	public static IEnumerable<IDocument> BlogPosts(IPipelineOutputs output)
+		=> Directory(output, "blog");
 
-		private static IEnumerable<IDocument> Directory(IPipelineOutputs output, string directory)
-			=> output.FromPipeline("Content")
-				.FilterSources($"{directory}/**/*.md")
-				.Where(d => d.GetBool("Show", true))
-				.OrderBy(d => CategoryOrder(d.GetString("Category")))
-				.ThenBy(Order)
-				.ToArray();
+	private static IEnumerable<IDocument> Directory(IPipelineOutputs output, string directory)
+		=> output.FromPipeline("Content")
+			.FilterSources($"{directory}/**/*.md")
+			.Where(d => d.GetBool("Show", true))
+			.OrderBy(d => CategoryOrder(d.GetString("Category")))
+			.ThenBy(Order)
+			.ToArray();
 
-		public static object JSON(IDocument doc)
-			=> "{" + string.Join(",", new[]
-				       {
-					       $@"name: ""{doc.GetString("Name")}""",
-					       $@"org: ""{doc.GetString("GitHubOrganization") ?? "ecoAPM"}""",
-					       !doc.GetString("Package").IsNullOrWhiteSpace() ? $@"package:""{doc.GetString("Package")}""" : null,
-					       !doc.GetString("Type").IsNullOrWhiteSpace() ? $@"type:""{doc.GetString("Type")}""" : null,
-					       !doc.GetString("CI").IsNullOrWhiteSpace() ? $@"CI:{doc.GetString("CI")}" : null,
-					       !doc.GetString("Tests").IsNullOrWhiteSpace() ? $@"tests:{doc.GetString("Tests")}" : null
-				       }.Where(s => s != null)
-			       )
-			       + "}";
+	public static object JSON(IDocument doc)
+		=> "{" + string.Join(",", new[]
+				   {
+						   $@"name: ""{doc.GetString("Name")}""",
+						   $@"org: ""{doc.GetString("GitHubOrganization") ?? "ecoAPM"}""",
+						   !doc.GetString("Package").IsNullOrWhiteSpace() ? $@"package:""{doc.GetString("Package")}""" : null,
+						   !doc.GetString("Type").IsNullOrWhiteSpace() ? $@"type:""{doc.GetString("Type")}""" : null,
+						   !doc.GetString("CI").IsNullOrWhiteSpace() ? $@"CI:{doc.GetString("CI")}" : null,
+						   !doc.GetString("Tests").IsNullOrWhiteSpace() ? $@"tests:{doc.GetString("Tests")}" : null
+				   }.Where(s => s != null)
+			   )
+			   + "}";
 
-		public static IEnumerable<IGrouping<string, IDocument>> SoftwareCategories(IPipelineOutputs outputs)
-			=> Software(outputs)
-				.GroupBy(d => d.GetString("Category"))
-				.OrderBy(c => CategoryOrder(c.Key));
+	public static IEnumerable<IGrouping<string, IDocument>> SoftwareCategories(IPipelineOutputs outputs)
+		=> Software(outputs)
+			.GroupBy(d => d.GetString("Category"))
+			.OrderBy(c => CategoryOrder(c.Key));
 
-		private static string Order(IDocument document)
-			=> document.GetInt(Keys.Order) > 0 ? document.GetInt(Keys.Order).ToString().PadLeft(2, '0')
-				: document.GetTitle().StartsWith("ecoAPM") ? $" {document.GetTitle()}"
-				: document.GetTitle();
+	private static string Order(IDocument document)
+		=> document.GetInt(Keys.Order) > 0 ? document.GetInt(Keys.Order).ToString().PadLeft(2, '0')
+			: document.GetTitle().StartsWith("ecoAPM") ? $" {document.GetTitle()}"
+			: document.GetTitle();
 
-		private static byte CategoryOrder(string category)
-			=> category switch
-			{
-				"ecoAPM" => 0,
-				"OSS Tools" => 1,
-				"Community Service" => 2,
-				_ => byte.MaxValue
-			};
+	private static byte CategoryOrder(string category)
+		=> category switch
+		{
+			"ecoAPM" => 0,
+			"OSS Tools" => 1,
+			"Community Service" => 2,
+			_ => byte.MaxValue
+		};
 
-		public static async Task<string> GetContent(IPipelineOutputs outputs, string key)
-			=> await outputs[key].First().GetContentStringAsync();
-	}
+	public static async Task<string> GetContent(IPipelineOutputs outputs, string key)
+		=> await outputs[key].First().GetContentStringAsync();
 }
--- Program.cs
+++ Program.cs
@@ -6,29 +6,29 @@ using Statiq.Common;
 using Statiq.Core;
 using Statiq.Web;
 
-namespace ecoAPM.Site
+namespace ecoAPM.Site;
+
+public static class Program
 {
-	public static class Program
-	{
-		public static async Task Main(string[] args)
-			=> await Bootstrapper.Factory
-				.CreateDefaultWithout(args, DefaultFeatures.Pipelines)
-				.AddWeb()
-				.AddPipeline("NPM", NodeModules)
-				.AddPipeline("SrcSet", ResponsiveImages)
-				.ModifyPipeline("Content", NonBreakingSlashes)
-				.ModifyPipeline("Content", NiceURLs)
-				.DeployToGitHubPages("ecoAPM", "ecoAPM.com", Config.FromSetting<string>("GITHUB_TOKEN"))
-				.RunAsync();
+	public static async Task Main(string[] args)
+		=> await Bootstrapper.Factory
+			.CreateDefaultWithout(args, DefaultFeatures.Pipelines)
+			.AddWeb()
+			.AddPipeline("NPM", NodeModules)
+			.AddPipeline("SrcSet", ResponsiveImages)
+			.ModifyPipeline("Content", NonBreakingSlashes)
+			.ModifyPipeline("Content", NiceURLs)
+			.DeployToGitHubPages("ecoAPM", "ecoAPM.com", Config.FromSetting<string>("GITHUB_TOKEN"))
+			.RunAsync();
 
-		private static void NonBreakingSlashes(IPipeline pipeline)
-			=> pipeline.ProcessModules.Add(new ReplaceInContent(" / ", _ => " / "));
+	private static void NonBreakingSlashes(IPipeline pipeline)
+		=> pipeline.ProcessModules.Add(new ReplaceInContent(" / ", _ => " / "));
 
-		private static void NiceURLs(IPipeline pipeline)
-			=> pipeline.ProcessModules.Add(new NiceURL());
+	private static void NiceURLs(IPipeline pipeline)
+		=> pipeline.ProcessModules.Add(new NiceURL());
 
-		private static IPipeline NodeModules(IReadOnlySettings arg)
-			=> new CopyFromNPM(new [] {
+	private static IPipeline NodeModules(IReadOnlySettings arg)
+		=> new CopyFromNPM(new[] {
 						"bootstrap/dist/css/bootstrap.min.css",
 						"bootstrap/dist/css/bootstrap.min.css.map",
 					 	"bootstrap/dist/js/bootstrap.min.js",
@@ -36,9 +36,8 @@ namespace ecoAPM.Site
 						"marked/marked.min.js",
 						"notosans/*",
 						"vue/dist/vue.global.prod.js"
-			});
+		});
 
-		private static IPipeline ResponsiveImages(IReadOnlySettings arg)
-			=> new ResponsiveImages("**/*.jpg");
-	}
+	private static IPipeline ResponsiveImages(IReadOnlySettings arg)
+		=> new ResponsiveImages("**/*.jpg");
 }
